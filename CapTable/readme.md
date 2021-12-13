62cdb845f700524c8e304ae0b270ad7c6b255f9f
Arkiralor <prithoo11335@gmail.com>
2021-12-14 01:25:12 +0530

    Packaged the thing.

---
+++ CapTable/readme.md
@@ -0,0 +1,6 @@
+# READ ME
+
+<p>
+Contains the models and methods to store the browserstack capabilities for each test-case
+into a db-table and to output those capabilities as a spreadsheet (MS .XLXS, to be precise).
+</p>
--- test_run.py
+++ test_run.py
@@ -1,7 +1,7 @@
 '''
 Use this script to test the functionality of the program.
 '''
-from methods import export_as_spreadsheet
+from CapTable.methods import export_as_spreadsheet
 
 if __name__ == '__main__':
     export_as_spreadsheet()
