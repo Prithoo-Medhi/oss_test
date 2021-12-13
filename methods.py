6fa45cf2b18c741957ae39b5ddbc8e91a586749d
Arkiralor <pritho11335@gmail.com>
2021-12-13 15:25:10 +0530

    added metadata to outputfile name.

--- methods.py
+++ methods.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import sqlite3
+from datetime import datetime
 
 def export_as_spreadsheet(tablename = 'cap_matrix', db = 'cap_blog.db'):
     """
@@ -10,6 +11,6 @@ def export_as_spreadsheet(tablename = 'cap_matrix', db = 'cap_blog.db'):
     # Get the data
     tabledata = pd.read_sql_query("SELECT * FROM " + tablename, conn)
     # Export to a spreadsheet
-    tabledata.to_excel('output/cap_matrix.xlsx', index = False)
+    tabledata.to_excel(f'output/{tablename}-{datetime.now()}.xlsx', index = False)
     # Close the connection
     conn.close()
