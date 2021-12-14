8760ffd38d023855c6fa82132da159b40717316a
Arkiralor <pritho11335@gmail.com>
2021-12-14 11:48:37 +0530

    Final packaging.

--- CapTable/methods.py
+++ CapTable/methods.py
@@ -16,6 +16,6 @@ def export_as_spreadsheet(tablename = 'cap_matrix', db = 'cap_log.db'):
     # Get the data
     tabledata = pd.read_sql_query("SELECT * FROM " + tablename, conn)
     # Export to a spreadsheet
-    tabledata.to_excel(f'{BASE_PATH}/captable_output/{tablename}-{datetime.now()}.xlsx', index = False)
+    tabledata.to_excel(f'captable_output/{tablename}.xlsx', index = False)
     # Close the connection
     conn.close()
